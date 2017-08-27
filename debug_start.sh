7251917c5fd2358a3e3e9d855bc994bdb2eb5efe
Team Rapture <teamrapture@portalnumber9>
2017-08-27 16:16:15 -0400

    Initial

---
+++ debug_start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#-XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50
+while true
+do
+
+    java -Dcom.sun.management.jmxremote.port=20427 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Xmx8G -XX:+UnlockExperimentalVMOptions -Dfml.doNotBackup=true -XX:ParallelGCThreads=8 -XX:+UseG1GC -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:+AggressiveOpts -XX:MaxGCPauseMillis=10 -XX:+UseStringDeduplication -XX:hashCode=5 -Dfile.encoding=UTF-8  -jar forge*.jar
+
+    echo Running Restart Script:
+    sleep 1
+
+    echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
+    echo "Rebooting in:"
+    for i in 10 9 8 7 6 5 4 3 2 1
+    do
+            echo "$i..."
+            sleep 1
+    done
+    echo "Rebooting now!"
+
+done
